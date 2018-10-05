cf11f774a7b906ae1946a4e53a8eab3f80a7e510
NB106 <NB106@users.noreply.github.com>
2018-10-05 14:18:01 +0930

    New Project
    Roles created, Authentication added

---
+++ assignment2/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using assignment2.Models;
+
+namespace assignment2.Data
+{
+        public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options) { }
+
+            public DbSet<Assignment2.Models.Coach> Coach { get; set; }
+        public DbSet<Assignment2.Models.Event> Event { get; set; }
+        public DbSet<Assignment2.Models.Member> Member { get; set; }
+        public DbSet<Assignment2.Models.Schedule> Schedule { get; set; }
+    }
+}
