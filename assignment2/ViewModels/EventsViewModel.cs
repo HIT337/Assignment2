2b2f0c491c5d2db0a5d9112ebeca12b11ea7b36e
PatrickA7 <patrick.alegria7@gmail.com>
2018-10-14 12:35:54 +0930

    Changed date of birth to read input, added view model for coach dropdown

---
+++ assignment2/ViewModels/EventsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using assignment2.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment2.ViewModels
+{
+    public class EventsViewModel
+    {
+        public string EventName { get; set; }
+
+        public Coach Model { get; set; }
+
+        public SelectList CoachSelectList { get; set; }
+
+        public EventsViewModel()
+        {
+            Model = new Coach();
+        }
+    }
+}
